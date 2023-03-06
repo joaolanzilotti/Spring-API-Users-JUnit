ae03e8a4d5a3aee027a2ebd94f71c6188851fce0
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-03-06 14:24:16 -0300

    Entity User implemented

--- src/main/java/com/jp/springapi/entities/User.java
+++ src/main/java/com/jp/springapi/entities/User.java
@@ -1,4 +1,41 @@
 package com.jp.springapi.entities;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tbl_user")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
---
+++ src/main/java/com/jp/springapi/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.jp.springapi.repositories;
+
+import com.jp.springapi.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
