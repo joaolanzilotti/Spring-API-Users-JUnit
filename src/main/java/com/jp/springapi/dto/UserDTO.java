ea5e603f061bc872dbed277f1c797e1c35d7bb42
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-03-07 11:59:54 -0300

    Dependecy ModelMapper added

---
+++ src/main/java/com/jp/springapi/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package com.jp.springapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String password;
+
+}
